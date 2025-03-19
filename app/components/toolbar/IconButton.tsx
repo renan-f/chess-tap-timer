819964a48df7d8ea92bb938ec1a7375f1ecda216
Renan Filgueiras <filgueiras.rfa@gmail.com>
2025-03-19 20:21:03 -0300

    permite passar estilo para o icone

--- app/components/toolbar/IconButton.tsx
+++ app/components/toolbar/IconButton.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import { TouchableOpacity, Text, View, StyleSheet } from 'react-native';
+import { TouchableOpacity, Text, StyleSheet, StyleProp, ViewStyle } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
 interface IProps {
+  style?: StyleProp<ViewStyle>,
   icon?: any,
   text?: string,
   onPress: () => void
 }
 
-const IconButton = ({ icon, text, onPress }: IProps) => {
+const IconButton = ({ icon, text, style, onPress }: IProps) => {
   return (
     <TouchableOpacity
-      style={[styles.button, { gap: 8 }]}
+      style={[styles.button, { gap: 8 }, style]}
       onPress={onPress}
     >
       {icon && <Ionicons name={icon} size={16} color="white" />}
